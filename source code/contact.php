c69b8fc3dae3ce88b14833bf5a41c31204748ea7
Harshad Bhosale <143777263+harshadbhosale21@users.noreply.github.com>
2023-09-29 15:40:40 +0530

    Add files via upload

---
+++ source code/contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Carnival 2k23</title>
+        
+        <head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class = "content"><!--body content holder-->
+            <div class = "container">
+                <div class = "col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1 style="color:#003300 ; font-size:38px ;"><strong>Contact Us</strong></h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+            
+            <div class="container">
+                <div class="col-md-6 contacts">
+                    <h1 style="color:#000080 ; font-size:42px ; font-style:bold "><span class="glyphicon glyphicon-user"></span> HARSHAD BHOSALE</h1>
+                    <p>
+                        <span class="glyphicon glyphicon-envelope"></span> Email: harshad@pcccs.ac.in<br>
+    
+                        <span class="glyphicon glyphicon-phone"></span> Mobile: 7777777777
+                    </p>
+                </div>
+                <div class="col-md-6 contacts">
+                    <h1 style="color:#000080 ; font-size:42px ; font-style:bold "><span class="glyphicon glyphicon-user"></span> YUSUF NAIKWADI</h1>
+                    <p>
+                        <span class="glyphicon glyphicon-envelope"></span> Email: yusuf@pcccs.ac.in<br>
+    
+                        <span class="glyphicon glyphicon-phone"></span> Mobile: 9999999999
+                    </p>
+                </div>
+            </div>
+			
+            
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+    </body>
+</html>
