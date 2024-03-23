ebcee5e0ac8623192a389cebaced41ea51085858
Harshad Bhosale <143777263+harshadbhosale21@users.noreply.github.com>
2024-03-23 14:51:22 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,39 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>P</strong><span class = "small footerSubtext">ratibha </span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>C</strong><span class = "small footerSubtext">merrce and</span>
+                        <strong>C</strong><span class = "small footerSubtext">omputer</span>
+                        <strong>S</strong><span class = "small footerSubtext">tudies</span>
+                    </p>
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: Kalbhor Nagar,
+                    Chinchwad,
+                    <br>PUNE, MAHARASHTRA </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/"><img src = "images/facebook.png"></a>
+                    <a href=""><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href="https://www.youtube.com/"><img src = "images/youtube.png" width="65px"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
